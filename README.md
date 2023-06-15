37e3974ce31b5d8e8da46712d97f0f53d26463b3
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-06-15 17:14:32 -0400

    Finshed

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # meditation app
  
+ Tutorial By  https://www.youtube.com/@JohnKomarnicki
+
+ Learned to create shapes 
+ Learned to expand javascript behavior knowleage 
+ Lsearned fuction shortcut 
+ Learned to make a responsive webpage
+
+
  Sources: Photo by <a href="https://unsplash.com/@nitinreddyk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nitin Karolla</a> on <a href="https://unsplash.com/photos/o78ytg6LbJc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let growCircle = () => {
     circleProgress.classList.add("circle-grow");
     setTimeout(() =>{
         circleProgress.classList.remove("circle-grow");
-    }, 8000);
+    }, 11000);
 };
 
 //Breathing Instructions
@@ -31,13 +31,40 @@ let breathingTextUpdate = () => {
         instructions.innerText ="Hold Breath";
         setTimeout(() =>{
             instructions.innerText ="Exhale Breath"
-        },4000)
-    }, 4000);
+        },8000)
+    }, ),7000;
 };
+
+
+
+
+//Breathing App Function 
+let breathinApp = () => {
+    let breathingAnimation = setInterval(() =>{
+        if(breathsLeft === 0){
+            clearInterval(breathingAnimation);
+            instructions.innerText = "Breathing session completed.Click 'Begin";
+            start.classList.remove('button-inactive');
+            breathsLeft = numberOfBreaths.value;
+            breathsText.innerText = breathsLeft;
+            return;
+        }
+        growCircle();
+        breathingTextUpdate();
+    }, 19000)
+}
 
 
 //same as onclick in html
 start.addEventListener('click', () =>{
-    growCircle();
-    breathingTextUpdate();
+    start.classList.add("button-inactive");
+    instructions.innerText = "Get Relaxed & ready to begin"
+    setTimeout(() =>{
+            instructions.innerText = "Breathing is about to begin";
+            setTimeout(() =>{
+                breathinApp();
+                growCircle();
+                breathingTextUpdate();
+            },2000);
+    },2000);
 });
